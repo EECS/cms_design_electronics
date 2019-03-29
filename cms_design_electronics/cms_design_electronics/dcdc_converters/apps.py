097b485d163cabdb3c74c0f439913f7760f57be6
Unknown <jacobshnn@gmail.com>
2019-03-29 14:36:02 -0700

    Restructuring project.

--- cms_design_electronics/cms_design_electronics/dcdc_converters/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DcdcConvertersConfig(AppConfig):
-    name = 'dcdc_converters'
--- cms_design_electronics/cms_design_electronics/design_center/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DesignCenterConfig(AppConfig):
-    name = 'design_center'
